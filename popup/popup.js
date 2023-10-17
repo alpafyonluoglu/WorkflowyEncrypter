bf45d6f74b7c3ba6e6387f040b0bbfe1ec44205b
Alp Afyonluoğlu <60400842+alpafyonluoglu@users.noreply.github.com>
2023-10-17 17:47:32 +0300

    Updated popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,16 @@
-console.log("Popup is up!");
+// Open links in new tab [https://stackoverflow.com/a/17732667]
+document.addEventListener('DOMContentLoaded', function () {
+    var links = document.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        (function () {
+            var ln = links[i];
+            var location = ln.href;
+            ln.onclick = function () {
+                chrome.tabs.create({active: true, url: location});
+            };
+        })();
+    }
+});
+
+// Show version number
+document.getElementById("version").innerText = chrome.runtime.getManifest().version;
