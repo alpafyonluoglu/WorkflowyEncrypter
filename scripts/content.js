8fa1eca1c4e3f8265f1ffaf7ba7f356263e5b3ca
Alp Afyonluoğlu <60400842+alpafyonluoglu@users.noreply.github.com>
2023-11-02 09:46:45 +0300

    Inject variable to document

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,5 @@
+// Inject variables
+injectVar("logoUrl", chrome.runtime.getURL('/src/logo_128.png'));
 injectScript(chrome.runtime.getURL('/scripts/lock.js'), 'body');
 
 // Inject script to page [https://gist.github.com/devjin0617/3e8d72d94c1b9e69690717a219644c7a]
@@ -7,4 +9,12 @@ function injectScript(file_path, tag) {
     script.setAttribute('type', 'text/javascript');
     script.setAttribute('src', file_path);
     node.appendChild(script);
+}
+
+function injectVar(key, value) {
+    var node = document.getElementsByTagName('body')[0];
+    var variable = document.createElement('span');
+    variable.id = "we-internal-" + key;
+    variable.setAttribute('value', value);
+    node.appendChild(variable);
 }
